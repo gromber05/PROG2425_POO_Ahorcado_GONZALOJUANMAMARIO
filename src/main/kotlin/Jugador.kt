0e17f9e3ad8f5c7f6e98c859edd1686c6d348a05
juanma-ge <jgomest545@g.educaand.es>
2025-01-24 19:31:57 +0100

    mario putero

--- src/main/kotlin/Jugador.kt
+++ src/main/kotlin/Jugador.kt
@@ -1,4 +1,26 @@
 package es.iesra.prog2425_ahorcado
 
-class Jugador {
+class Jugador(maxIntentos: Int, letrasUsadas: MutableList<Char>) {
+
+    private var intentos: Int = maxIntentos
+
+    private var letrasUsadas: MutableList<Char> = mutableListOf()
+
+    fun intentarLetra(letra: Char): Boolean {
+
+        if(letra !in letrasUsadas){
+            letrasUsadas.add(letra)
+            return true
+        } else {
+            return false
+        }
+    }
+
+    fun fallarIntento() {
+        if (intentos > 0)
+            intentos -= 1
+    }
+
+    fun obtenerLetrasUsadas() = letrasUsadas.joinToString(" ")
+
 }
