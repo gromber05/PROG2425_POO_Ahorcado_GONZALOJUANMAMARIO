21b6492e8a485b0864e5f83215cfa811118c40e6
Mario_Tamayo <trmario@iesrafaelalberti.es>
2025-01-24 19:21:42 +0100

    Avanzado la clase palabra

--- src/main/kotlin/Palabra.kt
+++ src/main/kotlin/Palabra.kt
@@ -7,5 +7,21 @@ import io.ktor.client.request.*
 import io.ktor.serialization.gson.*
 import kotlinx.coroutines.runBlocking
 
-class Palabra {
+class Palabra(val palabraOculta: String, private val progreso: ArrayList<Char>) {
+
+    fun revelarLetra(letra: Char) {
+        var i = 0
+        for (letraOculta in palabraOculta) {
+            i += 1
+            if (letra == letraOculta) {
+                progreso[i] = letra
+            }
+        }
+    }
+
+    fun obtenerProgreso() {
+        TODO()
+    }
+
+    fun esCompleta() = '_' !in progreso
 }
